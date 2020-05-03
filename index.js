126d70659cbec4528410147d5a89e5c7328743c2
BlakeFM <64390039+BlakeFM@users.noreply.github.com>
2020-05-02 18:03:27 -1000

    Made a counting sequence and added a picture

--- index.js
+++ index.js
@@ -4,18 +4,30 @@ let countDiv = null;
 let button2 = null;
 let sequenceDiv2 = null;
 let sequenceTextInput = null;
+let button3 = null;
+let countDiv2 = null;
+let countTextInput = null;
+let button4 = null;
 
 // Gets called on page load
 let onPageLoad = () => {
     // Set our UI elements
+
     button = document.getElementById("button1");
     countDiv = document.getElementById("count");
     button2 = document.getElementById("button2");
     sequenceDiv2 = document.getElementById("sequence");
     sequenceTextInput = document.getElementById("sequenceText");
+    button3 = document.getElementById("button3");
+    countDiv2 = document.getElementById("count1");
+    countTextInput = document.getElementById("countText")
+    button4 = document.getElementById("button4")
+
     // Set the button click event to run onButtonClick
     button.addEventListener("click", onButtonClick);
     button2.addEventListener("click", onButton2Click)
+    button3.addEventListener("click", onButton3Click)
+    button4.addEventListener("click", onButton4Click)
 }
 
 // Gets called on button click
@@ -23,18 +35,37 @@ let onButtonClick = () => {
     // Change the count div text
     countDiv.textContent = 'Yessah'
 }
+let onButton4Click = () => {
+    
+}
+
 let onButton2Click = () => {
     let totalRepeat = parseInt(sequenceTextInput.value);
-    let text="0       ";
-    let number1=BigInt(0);
-    let number2=BigInt(1);
-    for (let count = 1; count < totalRepeat; count+=1) {
-        let number3=number1+number2;
-        number1=number2;
-        number2=number3;
-        text += number3.toLocaleString()+"       ";
+    let text = "0       ";
+    let number1 = BigInt(0);
+    let number2 = BigInt(1);
+    for (let count = 1; count < totalRepeat; count += 1) {
+        let number3 = number1 + number2;
+        number1 = number2;
+        number2 = number3;
+        text += number3.toLocaleString() + "       ";
     }
     sequenceDiv2.textContent = text;
+}
+
+
+let onButton3Click = () => {
+    let Repeat = parseInt(countTextInput.value);
+    let text = "0     ";
+    BigInt(0);
+    let number5 = BigInt(0);
+    for (let count = 1; count < Repeat + 1; count += 1) {
+        let number6 ;
+        number6 = number5 + BigInt(1);
+        number5 = number5 + BigInt(1)
+        text += number6.toLocaleString() + "     ";
+    }
+    countDiv2.textContent = text;
 }
 // On page load, run the onPageLoad function
 document.addEventListener("DOMContentLoaded", onPageLoad);
